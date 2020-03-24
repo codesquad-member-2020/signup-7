33db6f77fd7f964198e07ee89209bc400c23615d
haveagood@naver.com <haveagood@naver.com>
2020-03-24 13:55:25 +0900

    <docs> : 백엔드 spring-boot 초기 세팅
    - spring data jdbc, spring web, mustach, spring-boot-devtools, gradle 세팅하였습니다
    
    - .gitignore 설정 : intellij, gradle, macos, java로 설정하였습니다
    
    # --COMMIT END--
    # <타입> 리스트
    # feat : 기능 (새로운 기능)
    # fix : 버그 (버그 수정)
    # refactor : 리팩토링
    # style : 스타일(코드 형식, 세미콜론 추가 : 비지니스 로직 변경 X)
    # docs : 문서 (문서 추가, 수정, 삭제)
    # test : 테스트 (테스트 코드 추가, 수정, 삭제 : 비지니스 로직 변경 X)
    # chroe : 기타 변경사항(빌드 스크립트 수정 등)
    -------------------------
    #	제목 첫 글자 대문자
    #	제목은 명령문
    #	제목 끝에 마침표(.) 금지
    #	제목과 본문을 한 줄 띄워 분리하기
    #	본문은 "어떻게" 보다 "무엇을" , "왜"를 설명한다
    #	본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    -------------------------

---
+++ be/src/main/java/com/codesquad/signupbackend/SignupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codesquad.signupbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignupBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignupBackendApplication.class, args);
+    }
+
+}
