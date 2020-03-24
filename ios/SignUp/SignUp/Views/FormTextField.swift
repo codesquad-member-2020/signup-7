33d17fef9c38334bf3c9ebdb78cc9eaacb50937b
seizze <cwcwc3@gmail.com>
2020-03-24 23:11:09 +0900

    [#13] feat: 텍스트필드에 인셋 추가

--- ios/SignUp/SignUp/Views/FormTextField.swift
+++ ios/SignUp/SignUp/Views/FormTextField.swift
@@ -1,5 +1,5 @@
 //
-//  TextField.swift
+//  FormTextField.swift
 //  SignUp
 //
 //  Created by Chaewan Park on 2020/03/24.
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class TextField: UITextField {
+@IBDesignable
+class FormTextField: UITextField {
     
     enum Appearance {
         case normal
         case focused
         case invalid
     }
+    
+    @IBInspectable var inset: CGFloat = 0
     
     var appearance: Appearance {
         didSet { setAppearance(to: appearance) }
@@ -30,11 +33,29 @@ class TextField: UITextField {
         super.init(coder: coder)
     }
     
+    override open func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: UIEdgeInsets(with: inset))
+    }
+    
+    override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: UIEdgeInsets(with: inset))
+    }
+    
+    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: UIEdgeInsets(with: inset))
+    }
+    
     func setAppearance(to appearance: Appearance) {
         switch appearance {
         case .normal: layer.borderColor = UIColor.borderGray.cgColor
         case .focused: layer.borderColor = UIColor.selectedBlue.cgColor
         case .invalid: layer.borderColor = UIColor.invalidRed.cgColor
         }
+    }
+}
+
+extension UIEdgeInsets {
+    init(with inset: CGFloat) {
+        self.init(top: inset, left: inset, bottom: inset, right: inset)
     }
 }
