763af807088ed9099120816d15f3d61911ade608
Jenny <huji3156@gmail.com>
2020-03-26 19:06:46 +0900

    for heroku test

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,8 @@
+// $("#checkId").click(validateId);
+//
+// function validateId(e) {
+//     e.preventDefault();
+//     console.log("button clicked")
+//     var userId = $("#userId").val();
+//     console.log("user id : " + userId);
+// }
