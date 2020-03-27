48602e05e189fa96e11449946b2f1f0760d8dac9
Jenny <huji3156@gmail.com>
2020-03-27 14:31:10 +0900

    재수정

--- src/main/java/com/example/group7/ApiUserController.java
+++ src/main/java/com/example/group7/ApiUserController.java
@@ -1,69 +1,57 @@
 package com.example.group7;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 @RestController
 public class ApiUserController {
-
     @Autowired
     UserRepository userRepository;
-
     ///회원가입
+    @CrossOrigin
     @PostMapping("/register")
     public ResponseEntity<ApiResponseMessage> createUser(User user) {
-
         ///아이디 중복 체크
         if (userRepository.isUserExists(user.getUserId())) {
-            return new ResponseEntity<>(new ApiResponseMessage(HttpStatus.BAD_REQUEST, "ID ALREADY IN USE"), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new ApiResponseMessage(HttpStatus.BAD_REQUEST, "ID ALREADY IN USE",404), HttpStatus.BAD_REQUEST);
         }
-
         if (userRepository.isNumberExists(user.getPhoneNumber())) {
-            return new ResponseEntity<>(new ApiResponseMessage(HttpStatus.BAD_REQUEST, "PHONE NUMBER ALREADY IN USE"), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new ApiResponseMessage(HttpStatus.BAD_REQUEST, "PHONE NUMBER ALREADY IN USE",404), HttpStatus.BAD_REQUEST);
         }
-
         if (userRepository.isEmailExists(user.getEmail())) {
-            return new ResponseEntity<>(new ApiResponseMessage(HttpStatus.BAD_REQUEST, "EMAIL ALREADY IN USE"), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new ApiResponseMessage(HttpStatus.BAD_REQUEST, "EMAIL ALREADY IN USE",404), HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(new ApiResponseMessage(HttpStatus.OK, "REGISTERED SUCCESSFULLY"), HttpStatus.OK);
+        return new ResponseEntity<>(new ApiResponseMessage(HttpStatus.OK, "REGISTERED SUCCESSFULLY",200), HttpStatus.OK);
     }
-
     ///아이디 중복검사
+    @CrossOrigin
     @GetMapping("/validate/id")
-    public ResponseEntity<ApiResponseMessage> isValidateId(String userId) {
-
+    public ResponseEntity<ApiResponseMessage> isValidateId(@RequestBody String userId) {
         if (!userRepository.isUserExists(userId)) {
             System.out.println("user id : " + userId);
             System.out.println(userRepository.isUserExists(userId));
-            return new ResponseEntity<>(new ApiResponseMessage(HttpStatus.OK, "ID AVAILABLE"), HttpStatus.OK);
+            return new ResponseEntity<>(new ApiResponseMessage(HttpStatus.OK, "ID AVAILABLE",200), HttpStatus.OK);
         }
-        return new ResponseEntity<>(new ApiResponseMessage(HttpStatus.BAD_REQUEST, "ID ALREADY IN USE"), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ApiResponseMessage(HttpStatus.BAD_REQUEST, "ID ALREADY IN USE",404), HttpStatus.BAD_REQUEST);
     }
-
     ///핸드폰 번호 중복검사
+    @CrossOrigin
     @GetMapping("/validate/phoneNumber")
-    public ResponseEntity<ApiResponseMessage> isValidateNumber(String phoneNumber) {
-
+    public ResponseEntity<ApiResponseMessage> isValidateNumber(@RequestBody String phoneNumber) {
         if (!userRepository.isNumberExists(phoneNumber)) {
             System.out.println("phone number : " + phoneNumber);
-            return new ResponseEntity<>(new ApiResponseMessage(HttpStatus.OK, "PHONE NUMBER AVAILABLE"), HttpStatus.OK);
+            return new ResponseEntity<>(new ApiResponseMessage(HttpStatus.OK, "PHONE NUMBER AVAILABLE",200), HttpStatus.OK);
         }
-        return new ResponseEntity<>(new ApiResponseMessage(HttpStatus.BAD_REQUEST, "PHONE NUMBER ALREADY IN USE"), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ApiResponseMessage(HttpStatus.BAD_REQUEST, "PHONE NUMBER ALREADY IN USE",404), HttpStatus.BAD_REQUEST);
     }
-
     ///이메일 중복검사
+    @CrossOrigin
     @GetMapping("/validate/email")
-    public ResponseEntity<ApiResponseMessage> isValidateEmail(String email) {
-
+    public ResponseEntity<ApiResponseMessage> isValidateEmail(@RequestBody String email) {
         if (!userRepository.isEmailExists(email)) {
             System.out.println("user email : " + email);
-            return new ResponseEntity<>(new ApiResponseMessage(HttpStatus.OK, "EMAIL AVAILABLE"), HttpStatus.OK);
+            return new ResponseEntity<>(new ApiResponseMessage(HttpStatus.OK, "EMAIL AVAILABLE",200), HttpStatus.OK);
         }
-        return new ResponseEntity<>(new ApiResponseMessage(HttpStatus.BAD_REQUEST, "EMAIL ALREADY IN USE"), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ApiResponseMessage(HttpStatus.BAD_REQUEST, "EMAIL ALREADY IN USE",404), HttpStatus.BAD_REQUEST);
     }
-
-
 }
